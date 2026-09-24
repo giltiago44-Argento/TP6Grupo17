05c8338139f75a7ad9882cb3963c6ceec7b03cd6
giltiago44-Argento <giltiago44@gmail.com>
2026-09-24 15:20:45 -0300

    Commit Inicial

---
+++ TP6Grupo17/src/main/java/com/mycompany/tp6grupo17/TP6Grupo17.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp6grupo17;
+
+/**
+ *
+ * @author Agos
+ */
+public class TP6Grupo17 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
